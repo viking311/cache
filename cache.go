d80cb541746dbf0d6e7cd149289cddd6b34d919a
viking311 <viking311@gmail.com>
2023-05-19 15:35:47 +0700

    rename pakege

---
+++ cache.go
@@ -0,0 +1,38 @@
+package cache
+
+import "sync"
+
+type Cache struct {
+	data map[string]interface{}
+	mx   sync.RWMutex
+}
+
+func (ch *Cache) Set(key string, value interface{}) {
+	defer ch.mx.Unlock()
+	ch.mx.Lock()
+	ch.data[key] = value
+}
+
+func (ch *Cache) Get(key string) interface{} {
+	defer ch.mx.RUnlock()
+	ch.mx.RLock()
+	if _, ok := ch.data[key]; ok {
+		return ch.data[key]
+	} else {
+		return nil
+	}
+}
+
+func (ch *Cache) Delete(key string) {
+	defer ch.mx.Unlock()
+	ch.mx.Lock()
+
+	delete(ch.data, key)
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string]interface{}),
+		mx:   sync.RWMutex{},
+	}
+}
